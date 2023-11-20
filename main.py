d34e8c30c897c672c0794113745e142c279b6c2c
tung.tq <tung.tq@teko.vn>
2023-11-20 14:31:14 +0700

    Remove roles

--- main.py
+++ main.py
@@ -18,23 +18,17 @@ total_item_counter = Counter(
 
 
 @dataclass
-class Role:
-    role_id: int
-
-
-@dataclass
 class User(msgspec.Struct):
     user_id: int
     name: str
     age: int
-    roles: List[Role]
 
     def get_id(self) -> int:
         return self.user_id
 
 
-u1 = User(user_id=11, name='user01', age=80, roles=[Role(role_id=51)])
-u2 = User(user_id=12, name='user02', age=81, roles=[Role(role_id=52)])
+u1 = User(user_id=11, name='user01', age=80)
+u2 = User(user_id=12, name='user02', age=81)
 
 
 @app.route('/users/<user_id>', methods=['GET'])
@@ -46,13 +40,13 @@ def get_user(user_id):
 
 
 def get_users_from_db(id_list: List[int]) -> List[User]:
-    return [User(user_id=i, name=f'users:{i}', age=51, roles=[Role(role_id=50 + i)]) for i in id_list]
+    return [User(user_id=i, name=f'users:{i}', age=51) for i in id_list]
 
 
 new_user_item = new_cache_item(
     cls=User, fill_func=get_users_from_db,
     get_key=User.get_id,
-    default=User(user_id=0, name='', age=0, roles=[]),
+    default=User(user_id=0, name='', age=0),
     key_name=lambda user_id: f'u2:{user_id}',
 )
 
@@ -64,22 +58,23 @@ def get_all_users():
     user_item = new_user_item()
 
     with tracer.start_span('get-from-cache') as span:
-        id_list = list(range(100))
+        id_list = list(range(10))
         fn = user_item.get_multi(id_list)
 
-        id_list = list(range(300, 320))
+        id_list = list(range(300, 310))
         fn2 = user_item.get_multi(id_list)
 
         users = fn()
         users2 = fn2()
 
-    with tracer.start_span('jsonify'):
-        obj = jsonify({
-            'users': users,
-            'users2': users2,
-        })
+        span.set_attribute('hit_count', user_item.hit_count)
+        span.set_attribute('fill_count', user_item.fill_count)
+        span.set_attribute('bytes_read', user_item.bytes_read)
 
-    return obj
+    return jsonify({
+        'users': users,
+        'users2': users2,
+    })
 
 
 @api.route('/customers')
